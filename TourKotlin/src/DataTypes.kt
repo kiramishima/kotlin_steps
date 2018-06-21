d59a3312874a942dfd0dd103b94d5d19021c858b
Kira_Mishima <kiramishima@outlook.com>
2018-06-21 03:02:49 -0500

    Update Local with Remote

---
+++ TourKotlin/src/DataTypes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    // var x = 10
+    // var y = 12
+    // var name = "Kira"
+    // name = "Kira Mishima"
+    // read only use val
+    val name = "Kira"
+    val age:Int = 29
+    val dep:String?
+    dep = "Software Engineering"
+    println("name:" + name)
+    println("age:" + age)
+    println("deparment:" + dep)
+    val pi:Double = 3.4
+}
---
+++ TourKotlin/src/FirstApp.kt
@@ -0,0 +1,7 @@
+
+
+fun main(args:Array<String>) {
+    print("Hi Kira,\n")
+    print("Welcome to Kotlin")
+    print(3)
+}
